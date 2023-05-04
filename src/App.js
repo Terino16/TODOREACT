92007860524ca1e4c67cffe92e01a92cf670186c
Terino16 <123401184+Terino16@users.noreply.github.com>
2023-05-05 03:16:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Task from './Task'
 
 function App() {
+  const [todolist, settodolist] = useState([]);
+  const [newtask, setnewtask] = useState("");
+   
+  const handleaddtask=()=>{
+    const task={
+      id: todolist.length===0 ? 1 : todolist.length+1,
+      Taskname: newtask,
+      complete:false
+    }
+    settodolist(task.taskName !== "" ? [...todolist, task] : todolist);
+  }
+
+  const handeldelete=(id)=>{
+    settodolist(todolist.filter((task) => task.id !== id));
+  }
+
+  const handelcomplete=(id)=>{
+    settodolist(todolist.filter((task) => task.id !== id));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App ">
+    <div className='flex items-center justify-center mt-10'>
+      <input className='border-2 border-rose-500 w-[50%] h-[3rem]'  value={newtask} onChange={(event)=>setnewtask(event.target.value)}/>
+      <button className='mx-2 p-3 bg-blue-300' onClick={handleaddtask}>ADD TASK</button>
+    </div>
+    <div>
+      {todolist.map((task)=>{
+        return(<Task 
+        id={task.id}
+        Taskname={task.Taskname}
+        handeldelete={handeldelete}
+        handelcomplete={handelcomplete}
+        />)
+      })}
+    </div>
     </div>
   );
 }
